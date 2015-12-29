d36bbf604bb5c728bc741a6805f5b5b56a56771d
lifeMiik <lifeMiik@huangrenbodemacbook-air.local>
2015-12-29 09:39:25 +0800

    Initial Commit

---
+++ SecondCommit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SecondCommit
+//
+//  Created by lifeMiik on 15/12/29.
+//  Copyright © 2015年 lifeMiik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SecondCommitTests/SecondCommitTests.m
@@ -0,0 +1,39 @@
+//
+//  SecondCommitTests.m
+//  SecondCommitTests
+//
+//  Created by lifeMiik on 15/12/29.
+//  Copyright © 2015年 lifeMiik. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SecondCommitTests : XCTestCase
+
+@end
+
+@implementation SecondCommitTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
